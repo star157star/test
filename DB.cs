e14dcdf39db2899a1aff7e06e60a95d54c3f3079
star157star <star157star@gmail.com>
2017-03-24 10:07:27 +0800

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace PTT_Crawler
+{
+    class DB
+    {
+        SqlConnection myConn;
+
+        public DB(string dbHost, string dbUser, string dbPass, string dbName)
+        {
+            string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName + ";charset=utf8;";
+            myConn = new SqlConnection(connStr);
+        }
+
+        public void open()
+        {
+            myConn.Open();
+        }
+
+        public void insertData(string tableName, string field, string value)
+        {
+            SqlCommand cmd = myConn.CreateCommand();
+            cmd.CommandText = "INSERT INTO " + tableName + "(`" + field + "`) VALUES ('" + value + "')"; ;
+            cmd.ExecuteNonQuery();
+        }
+
+        public void close()
+        {
+            myConn.Close();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Windows.Forms;
+
+namespace PTT_Crawler
+{
+    public partial class Form1 : Form
+    {
+        DB db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new DB("localhost", "root", "", "ptt-crawler-db");
+            db.open();
+        }
+
+        private void insertData_Click(object sender, EventArgs e)
+        {
+            db.insertData("test", "test", "你好");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("你是否確定要離開?", "警告", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                db.close();
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void crawPTT_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver(".");
+            driver.Navigate().GoToUrl("https://www.ptt.cc/bbs/NBA/index4539.html");
+            for (int i = 1; i <= 20; i++)
+            {
+                var title = driver.FindElement(By.XPath("/html/body/div[@id='main-container']/div[@class='r-list-container bbs-screen']/div[@class='r-ent'][" + i + "]/div[@class='title']/a"));
+                db.insertData("test", "test", title.GetAttribute("innerHTML").ToString());
+            }
+
+            driver.Quit();
+        }
+    }
+}
